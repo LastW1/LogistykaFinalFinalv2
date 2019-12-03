2f71c859353c75a579e6d395f90af96c1c1c20c1
przbog123@wp.pl <przbog123@wp.pl>
2019-12-03 16:26:27 +0100

    xD

---
+++ Logistyka2_staging_0.0/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logistyka2_staging_0._0
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            try
+            {
+                pictureBox1.ImageLocation = @"graph.png";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("nie załadowano grafu");
+                return;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
